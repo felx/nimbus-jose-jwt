98f29f09ccb1c0e14e01d8223dd35976e01ae5c1
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-09-26 10:03:02 +0300

    Adds JWSHeader JUnit test

---
+++ test/com/nimbusds/jose/sdk/JWSHeaderTest.java
@@ -0,0 +1,67 @@
+package com.nimbusds.jose.sdk;
+
+
+import java.text.ParseException;
+
+import junit.framework.TestCase;
+
+import com.nimbusds.jose.sdk.util.Base64URL;
+
+
+/**
+ * Tests JWS header parsing and serialisation.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-03-26)
+ */
+public class JWSHeaderTest extends TestCase {
+	
+	
+	public void testParseJSONText() {
+	
+		// Example header from JWS spec
+		
+		String s = "{\"typ\":\"JWT\",\"alg\":\"HS256\"}";
+		
+		JWSHeader h = null;
+		
+		try {
+			h = JWSHeader.parse(s);
+			
+		} catch (ParseException e) {
+		
+			fail(e.getMessage());
+		}
+		
+		assertNotNull(h);
+		
+		assertEquals(new JOSEObjectType("JWT"), h.getType());
+		assertEquals(JWSAlgorithm.HS256, h.getAlgorithm());
+		assertNull(h.getContentType());
+	}
+	
+	
+	public void testParseBase64URLText() {
+	
+		// Example header from JWS spec
+		
+		String s = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9";
+		
+		JWSHeader h = null;
+		
+		try {
+			h = JWSHeader.parse(new Base64URL(s));
+			
+		} catch (ParseException e) {
+		
+			fail(e.getMessage());
+		}
+		
+		assertNotNull(h);
+		
+		assertEquals(new JOSEObjectType("JWT"), h.getType());
+		assertEquals(JWSAlgorithm.HS256, h.getAlgorithm());
+		assertNull(h.getContentType());
+	}
+}
+
