7bef511fb27154379270f2600ba1ddd56464fcaa
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-09-28 16:02:45 +0300

    Updates PlainJWT JUnit test

--- test/com/nimbusds/jwt/PlainJWTTest.java
+++ test/com/nimbusds/jwt/PlainJWTTest.java
@@ -12,7 +12,7 @@ import com.nimbusds.jose.sdk.util.Base64URL;
  * Tests plain JWT object. Uses test vectors from JWT spec.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2012-09-27)
+ * @version $version$ (2012-09-28)
  */
 public class PlainJWTTest extends TestCase {
 
@@ -28,6 +28,31 @@ public class PlainJWTTest extends TestCase {
 		                                 "cGxlLmNvbS9pc19yb290Ijp0cnVlfQ");
 	
 		PlainJWT jwt = new PlainJWT(part1, part2);
+		
+		assertEquals(Algorithm.NONE, jwt.getHeader().getAlgorithm());
+		assertNull(jwt.getHeader().getType());
+		assertNull(jwt.getHeader().getContentType());
+		
+		ReadOnlyClaimsSet cs = jwt.getClaimsSet();
+		
+		assertEquals("joe", cs.getIssuerClaim());
+		assertEquals(1300819380, cs.getExpirationTimeClaim());
+		assertTrue((Boolean)cs.getCustomClaim("http://example.com/is_root"));
+	}
+	
+	
+	public void testParse()
+		throws Exception {
+	
+		String s = "eyJhbGciOiJub25lIn0" +
+		           "." +
+			   "eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt" +
+			   "cGxlLmNvbS9pc19yb290Ijp0cnVlfQ" +
+			   ".";
+	
+		PlainJWT jwt = PlainJWT.parse(s);
+		
+		assertNotNull(jwt);
 		
 		assertEquals(Algorithm.NONE, jwt.getHeader().getAlgorithm());
 		assertNull(jwt.getHeader().getType());
