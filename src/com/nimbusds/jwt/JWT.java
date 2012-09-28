2fe7d04b79b1ab8b442d9083b04252a9ab0a6430
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-09-28 15:18:29 +0300

    Extends JWT interface with getHeader and serialize methods

--- src/com/nimbusds/jwt/JWT.java
+++ src/com/nimbusds/jwt/JWT.java
@@ -3,16 +3,26 @@ package com.nimbusds.jwt;
 
 import java.text.ParseException;
 
+import com.nimbusds.jose.sdk.ReadOnlyHeader;
+
 
 /**
  * JSON Web Token (JWT) interface.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2012-09-25)
+ * @version $version$ (2012-09-28)
  */
 public interface JWT {
 
 
+	/**
+	 * Gets the JOSE header of the JSON Web Token (JWT).
+	 *
+	 * @return The header.
+	 */
+	public ReadOnlyHeader getHeader();
+	
+	 
 	/**
 	 * Gets the claims set of the JSON Web Token (JWT).
 	 *
@@ -25,4 +35,16 @@ public interface JWT {
 	 */
 	public ReadOnlyClaimsSet getClaimsSet()
 		throws ParseException;
+	
+	
+	/**
+	 * Serialises the JSON Web Token (JWT) to its compact format consisting 
+	 * of Base64URL-encoded parts delimited by period ('.') characters.
+	 *
+	 * @return The serialised JWT.
+	 *
+	 * @throws IllegalStateException If the JOSE object is not in a state 
+	 *                               that permits serialisation.
+	 */
+	public String serialize();
 }
