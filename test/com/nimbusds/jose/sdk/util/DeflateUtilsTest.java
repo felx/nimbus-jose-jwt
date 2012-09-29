55ffddf1bf05166b63335c0c5ea82d5c54c12292
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-09-29 09:44:26 +0300

    Updates DeflateUtils JUnit test

--- test/com/nimbusds/jose/sdk/util/DeflateUtilsTest.java
+++ test/com/nimbusds/jose/sdk/util/DeflateUtilsTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestCase;
 /**
  * Tests DEFLATE compression.
  *
- * @version $version$ (2012-09-25)
+ * @version $version$ (2012-09-29)
  */
 public class DeflateUtilsTest extends TestCase {
 	
@@ -15,7 +15,7 @@ public class DeflateUtilsTest extends TestCase {
 	public void testRun()
 		throws Exception {
 	
-		final String text = "abc123";
+		final String text = "Hello world!";
 		final byte[] textBytes = text.getBytes("UTF-8");
 			
 		byte[] compressed = DeflateUtils.compress(textBytes);
@@ -23,7 +23,10 @@ public class DeflateUtilsTest extends TestCase {
 		byte[] textBytesDecompressed = DeflateUtils.decompress(compressed);
 		String textDecompressed = new String(textBytesDecompressed, "UTF-8");
 		
-		assertEquals(text.length(), textDecompressed.length());
-		assertEquals(text, textDecompressed);
+		assertEquals("byte length check", textBytes.length, textBytesDecompressed.length);
+		
+		assertEquals("text length check", text.length(), textDecompressed.length());
+		
+		assertEquals("text comparison", text, textDecompressed);
 	}
 }
