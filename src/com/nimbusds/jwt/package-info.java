16938e48a683842bb4a8cf81344a08c8d02af101
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-10-07 17:24:27 +0300

    Updates JavaDocs

--- src/com/nimbusds/jwt/package-info.java
+++ src/com/nimbusds/jwt/package-info.java
@@ -1,6 +1,15 @@
 /**
  * JSON Web Token (JWT) classes.
  *
+ * <p>This package provides representation, compact serialisation and parsing 
+ * for the following JWT objects:
+ *
+ * <ul>
+ *     <li>{@link com.nimbusds.jwt.PlainJWT Plain JWTs}.
+ *     <li>{@link com.nimbusds.jwt.SignedJWT Signed JWTs}.
+ *     <li>{@link com.nimbusds.jwt.EncryptedJWT Encrypted JWTs}.
+ * </ul>
+ *
  * <p>References:
  *
  * <ul>
