d92d6d416716d6c3e77f933ffea136bdf2cb84d8
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-10-16 11:29:51 +0300

    Updates JWEHeaderFilter interface

--- src/com/nimbusds/jose/JWEHeaderFilter.java
+++ src/com/nimbusds/jose/JWEHeaderFilter.java
@@ -6,11 +6,10 @@ import java.util.Set;
 
 /**
  * JSON Web Encryption (JWE) header filter. Specifies accepted JWE algorithms,
- * encryption methods, integrity algorithms, key derivation functions and header
- * parameters.
+ * encryption methods, and header parameters.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2012-10-04)
+ * @version $version$ (2012-10-16)
  */
 public interface JWEHeaderFilter extends HeaderFilter {
 
@@ -55,46 +54,4 @@ public interface JWEHeaderFilter extends HeaderFilter {
 	 *                     {@code null}.
 	 */
 	public void setAcceptedEncryptionMethods(final Set<EncryptionMethod> acceptedEncs);
-	
-	
-	/**
-	 * Gets the names of the accepted integrity algorithms. These correspond
-	 * to the optional {@code int} JWE header parameter.
-	 *
-	 * @return The accepted integrity algorithms as a read-only set, empty 
-	 *         set if none.
-	 */
-	public Set<JWSAlgorithm> getAcceptedIntegrityAlgorithms();
-	
-	
-	/**
-	 * Sets the names of the accepted integrity algorithms. These correspond
-	 * to the optional {@code int} JWE header parameter.
-	 *
-	 * @param acceptedInts The accepted integrity algorithms. Must be a 
-	 *                     subset of the supported integrity algorithms and
-	 *                     not {@code null}.
-	 */
-	public void setAcceptedIntegrityAlgorithms(final Set<JWSAlgorithm> acceptedInts);
-	
-	
-	/**
-	 * Gets the names of the accepted key derivation functions. These 
-	 * correspond to the optional {@code kdf} JWE header parameter.
-	 *
-	 * @return The accepted key derivation functions as a read-only set, 
-	 *         empty set if none.
-	 */
-	public Set<KeyDerivationFunction> getAcceptedKeyDerivationFunctions();
-	
-	
-	/**
-	 * Sets the names of the accepted key derivation functions. These 
-	 * correspond to the optional {@code kdf} JWE header parameter.
-	 *
-	 * @param acceptedKdfs The accepted key derivation functions. Must be a
-	 *                     subset of the supported key derivation functions
-	 *                     and not {@code null}.
-	 */
-	public void setAcceptedKeyDerivationFunctions(final Set<KeyDerivationFunction> acceptedKdfs);
 }
