46b6438a020b96e0d4551213fb499ebd8d6ec1c0
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-12-17 10:06:17 +0100

    Adds getParsedParts() and getParsedString() methods to JWT interface

--- src/main/java/com/nimbusds/jwt/JWT.java
+++ src/main/java/com/nimbusds/jwt/JWT.java
@@ -5,12 +5,14 @@ import java.text.ParseException;
 
 import com.nimbusds.jose.ReadOnlyHeader;
 
+import com.nimbusds.jose.util.Base64URL;
+
 
 /**
  * JSON Web Token (JWT) interface.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2012-09-28)
+ * @version $version$ (2012-12-17)
  */
 public interface JWT {
 
@@ -29,12 +31,35 @@ public interface JWT {
 	 * @return The claims set, {@code null} if not available (for an 
 	 *         encrypted JWT that isn't decrypted).
 	 *
-	 * @throws ParseException If payload of the plain/JWS/JWE object doesn't
-	 *                        represent a valid JSON object and a JWT claims
-	 *                        set.
+	 * @throws ParseException If the payload of the JWT doesn't represent a
+	 *                        valid JSON object and a JWT claims set.
 	 */
 	public ReadOnlyClaimsSet getClaimsSet()
 		throws ParseException;
+
+
+	/**
+	 * Gets the original parsed Base64URL parts used to create the JSON Web
+	 * Token (JWT).
+	 *
+	 * @return The original Base64URL parts used to creates the JWT,
+	 *         {@code null} if the JWT was created from scratch. The 
+	 *         individual parts may be empty or {@code null} to indicate a 
+	 *         missing part.
+	 */
+	public Base64URL[] getParsedParts();
+
+
+	/**
+	 * Gets the original parsed string used to create the JSON Web Token 
+	 * (JWT).
+	 *
+	 * @see #getParsedParts
+	 * 
+	 * @return The parsed string used to create the JWT, {@code null} if 
+	 *         the JWT was created from scratch.
+	 */
+	public String getParsedString();
 	
 	
 	/**
@@ -43,8 +68,8 @@ public interface JWT {
 	 *
 	 * @return The serialised JWT.
 	 *
-	 * @throws IllegalStateException If the JOSE object is not in a state 
-	 *                               that permits serialisation.
+	 * @throws IllegalStateException If the JWT is not in a state that 
+	 *                               permits serialisation.
 	 */
 	public String serialize();
 }
