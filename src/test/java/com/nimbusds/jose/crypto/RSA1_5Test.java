caa57491ce2345559420505b303340b083f230f9
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-02-12 10:30:01 +0200

    Switches to test RSA key from JWE spec (Appendix 2 / RSA1-5)

--- src/test/java/com/nimbusds/jose/crypto/RSA1_5Test.java
+++ src/test/java/com/nimbusds/jose/crypto/RSA1_5Test.java
@@ -23,7 +23,8 @@ import com.nimbusds.jose.Payload;
 
 
 /**
- * Tests RSA1-5 JWE encryption and decryption.
+ * Tests RSA1-5 JWE encryption and decryption. Uses test RSA keys from the JWE
+ * spec.
  *
  * @author Vladimir Dzhuvinov
  * @version $version$ (2013-02-12)
@@ -31,75 +32,77 @@ import com.nimbusds.jose.Payload;
 public class RSA1_5Test extends TestCase {
 
 
-	private final static byte[] mod = { (byte)161, (byte)248, (byte) 22, (byte) 10, (byte)226, (byte)227, (byte)201, (byte)180,
-                                            (byte)101, (byte)206, (byte)141, (byte) 45, (byte)101, (byte) 98, (byte) 99, (byte) 54, 
-					    (byte) 43, (byte)146, (byte)125, (byte)190, (byte) 41, (byte)225, (byte)240, (byte) 36, 
-					    (byte)119, (byte)252, (byte) 22, (byte) 37, (byte)204, (byte)144, (byte)161, (byte) 54, 
-					    (byte)227, (byte)139, (byte)217, (byte) 52, (byte)151, (byte)197, (byte)182, (byte)234, 
-					    (byte) 99, (byte)221, (byte)119, (byte) 17, (byte)230, (byte)124, (byte)116, (byte) 41, 
-					    (byte)249, (byte) 86, (byte)176, (byte)251, (byte)138, (byte)143, (byte)  8, (byte)154, 
-					    (byte)220, (byte) 75, (byte)105, (byte)137, (byte) 60, (byte)193, (byte) 51, (byte) 63, 
-					    (byte) 83, (byte)237, (byte)208, (byte) 25, (byte)184, (byte)119, (byte)132, (byte) 37, 
-					    (byte) 47, (byte)236, (byte)145, (byte) 79, (byte)228, (byte)133, (byte)119, (byte)105,
-					    (byte) 89, (byte) 75, (byte)234, (byte) 66, (byte)128, (byte)211, (byte) 44, (byte) 15, 
-					    (byte) 85, (byte)191, (byte) 98, (byte)148, (byte) 79, (byte) 19, (byte)  3, (byte)150, 
-					    (byte)188, (byte)110, (byte)155, (byte)223, (byte)110, (byte)189, (byte)210, (byte)189, 
-					    (byte)163, (byte)103, (byte)142, (byte)236, (byte)160, (byte)198, (byte)104, (byte)247, 
-					    (byte)  1, (byte)179, (byte)141, (byte)191, (byte)251, (byte) 56, (byte)200, (byte) 52, 
-					    (byte) 44, (byte)226, (byte)254, (byte)109, (byte) 39, (byte)250, (byte)222, (byte) 74, 
-					    (byte) 90, (byte) 72, (byte)116, (byte)151, (byte)157, (byte)212, (byte)185, (byte)207, 
-					    (byte)154, (byte)222, (byte)196, (byte)199, (byte) 91, (byte)  5, (byte)133, (byte) 44, 
-					    (byte) 44, (byte) 15, (byte) 94, (byte)248, (byte)165, (byte)193, (byte)117, (byte)  3,
-					    (byte)146, (byte)249, (byte) 68, (byte)232, (byte)237, (byte)100, (byte)193, (byte) 16, 
-					    (byte)198, (byte)182, (byte) 71, (byte) 96, (byte)154, (byte)164, (byte)120, (byte) 58, 
-					    (byte)235, (byte)156, (byte)108, (byte)154, (byte)215, (byte) 85, (byte) 49, (byte) 48, 
-					    (byte) 80, (byte) 99, (byte)139, (byte)131, (byte)102, (byte) 92, (byte)111, (byte)111, 
-					    (byte)122, (byte)130, (byte)163, (byte)150, (byte)112, (byte) 42, (byte) 31, (byte)100, 
-					    (byte) 27, (byte)130, (byte)211, (byte)235, (byte)242, (byte) 57, (byte) 34, (byte) 25, 
-					    (byte) 73, (byte) 31, (byte)182, (byte)134, (byte)135, (byte) 44, (byte) 87, (byte) 22, 
-					    (byte)245, (byte) 10, (byte)248, (byte) 53, (byte)141, (byte)154, (byte)139, (byte)157, 
-					    (byte) 23, (byte)195, (byte) 64, (byte)114, (byte)143, (byte)127, (byte)135, (byte)216,
-					    (byte)154, (byte) 24, (byte)216, (byte)252, (byte)171, (byte)103, (byte)173, (byte)132, 
-					    (byte) 89, (byte) 12, (byte) 46, (byte)207, (byte)117, (byte)147, (byte) 57, (byte) 54, 
-					    (byte) 60, (byte)  7, (byte)  3, (byte) 77, (byte)111, (byte) 96, (byte)111, (byte)158, 
-					    (byte) 33, (byte)224, (byte) 84, (byte) 86, (byte)202, (byte)229, (byte)233, (byte)161  };
+	private final static byte[] mod = { 
+		(byte)177, (byte)119, (byte) 33, (byte) 13, (byte)164, (byte) 30, (byte)108, (byte)121, 
+		(byte)207, (byte)136, (byte)107, (byte)242, (byte) 12, (byte)224, (byte) 19, (byte)226, 
+		(byte)198, (byte)134, (byte) 17, (byte) 71, (byte)173, (byte) 75, (byte) 42, (byte) 61, 
+		(byte) 48, (byte)162, (byte)206, (byte)161, (byte) 97, (byte)108, (byte)185, (byte)234, 
+		(byte)226, (byte)219, (byte)118, (byte)206, (byte)118, (byte)  5, (byte)169, (byte)224, 
+		(byte) 60, (byte)181, (byte) 90, (byte) 85, (byte) 51, (byte)123, (byte)  6, (byte)224, 
+		(byte)  4, (byte)122, (byte) 29, (byte)230, (byte)151, (byte) 12, (byte)244, (byte)127, 
+		(byte)121, (byte) 25, (byte)  4, (byte) 85, (byte)220, (byte)144, (byte)215, (byte)110, 
+		(byte)130, (byte) 17, (byte) 68, (byte)228, (byte)129, (byte)138, (byte)  7, (byte)130, 
+		(byte)231, (byte) 40, (byte)212, (byte)214, (byte) 17, (byte)179, (byte) 28, (byte)124,     
+		(byte)151, (byte)178, (byte)207, (byte) 20, (byte) 14, (byte)154, (byte)222, (byte)113, 
+		(byte)176, (byte) 24, (byte)198, (byte) 73, (byte)211, (byte)113, (byte)  9, (byte) 33, 
+		(byte)178, (byte) 80, (byte) 13, (byte) 25, (byte) 21, (byte) 25, (byte)153, (byte)212, 
+		(byte)206, (byte) 67, (byte)154, (byte)147, (byte) 70, (byte)194, (byte)192, (byte)183, 
+		(byte)160, (byte) 83, (byte) 98, (byte)236, (byte)175, (byte) 85, (byte) 23, (byte) 97, 
+		(byte) 75, (byte)199, (byte)177, (byte) 73, (byte)145, (byte) 50, (byte)253, (byte)206, 
+		(byte) 32, (byte)179, (byte)254, (byte)236, (byte)190, (byte) 82, (byte) 73, (byte) 67, 
+		(byte)129, (byte)253, (byte)252, (byte)220, (byte)108, (byte)136, (byte)138, (byte) 11, 
+		(byte)192, (byte)  1, (byte) 36, (byte)239, (byte)228, (byte) 55, (byte) 81, (byte)113, 
+		(byte) 17, (byte) 25, (byte)140, (byte) 63, (byte)239, (byte)146, (byte)  3, (byte)172,  
+		(byte) 96, (byte) 60, (byte)227, (byte)233, (byte) 64, (byte)255, (byte)224, (byte)173, 
+		(byte)225, (byte)228, (byte)229, (byte) 92, (byte)112, (byte) 72, (byte) 99, (byte) 97, 
+		(byte) 26, (byte) 87, (byte)187, (byte)123, (byte) 46, (byte) 50, (byte) 90, (byte)202, 
+		(byte)117, (byte) 73, (byte) 10, (byte)153, (byte) 47, (byte)224, (byte)178, (byte)163, 
+		(byte) 77, (byte) 48, (byte) 46, (byte)154, (byte) 33, (byte)148, (byte) 34, (byte)228, 
+		(byte) 33, (byte)172, (byte)216, (byte) 89, (byte) 46, (byte)225, (byte)127, (byte) 68, 
+		(byte)146, (byte)234, (byte) 30, (byte)147, (byte) 54, (byte)146, (byte)  5, (byte)133, 
+		(byte) 45, (byte) 78, (byte)254, (byte) 85, (byte) 55, (byte) 75, (byte)213, (byte) 86, 
+		(byte)194, (byte)218, (byte)215, (byte)163, (byte)189, (byte)194, (byte) 54, (byte)  6, 
+		(byte) 83, (byte) 36, (byte) 18, (byte)153, (byte) 53, (byte)  7, (byte) 48, (byte) 89, 
+		(byte) 35, (byte) 66, (byte)144, (byte)  7, (byte) 65, (byte)154, (byte) 13, (byte) 97, 
+		(byte) 75, (byte) 55, (byte)230, (byte)132, (byte)  3, (byte) 13, (byte)239, (byte) 71  };
 
 		  
 	private static final byte[] exp= { 1, 0, 1 };
 	
 	
-	private static final byte[] modPriv = { (byte)  18, (byte) 174, (byte) 113, (byte) 164, (byte) 105, (byte) 205, (byte)  10, (byte)  43,
-	                                        (byte) 195, (byte) 126, (byte)  82, (byte) 108, (byte)  69, (byte)   0, (byte)  87, (byte)  31, 
-						(byte)  29, (byte)  97, (byte) 117, (byte)  29, (byte) 100, (byte) 233, (byte)  73, (byte) 112, 
-						(byte) 123, (byte)  98, (byte)  89, (byte)  15, (byte) 157, (byte)  11, (byte) 165, (byte) 124, 
-						(byte) 150, (byte)  60, (byte)  64, (byte)  30, (byte)  63, (byte) 207, (byte)  47, (byte)  44, 
-						(byte) 211, (byte) 189, (byte) 236, (byte) 136, (byte) 229, (byte)   3, (byte) 191, (byte) 198, 
-						(byte)  67, (byte) 155, (byte)  11, (byte)  40, (byte) 200, (byte)  47, (byte) 125, (byte)  55, 
-						(byte) 151, (byte) 103, (byte)  31, (byte)  82, (byte)  19, (byte) 238, (byte) 216, (byte) 193, 
-						(byte)  90, (byte)  37, (byte) 216, (byte) 213, (byte) 206, (byte) 160, (byte)   2, (byte)  94, 
-						(byte) 227, (byte) 171, (byte)  46, (byte) 139, (byte) 127, (byte) 121, (byte)  33, (byte) 111,
-						(byte) 198, (byte)  59, (byte) 234, (byte)  86, (byte)  39, (byte)  83, (byte) 180, (byte)  6, 
-						(byte)  68, (byte) 198, (byte) 161, (byte)  81, (byte)  39, (byte) 217, (byte) 178, (byte) 149, 
-						(byte)  69, (byte)  64, (byte) 160, (byte) 187, (byte) 225, (byte) 163, (byte)   5, (byte)  86, 
-						(byte) 152, (byte)  45, (byte)  78, (byte) 159, (byte) 222, (byte)  95, (byte) 100, (byte)  37, 
-						(byte) 241, (byte)  77, (byte)  75, (byte) 113, (byte)  52, (byte)  65, (byte) 181, (byte)  93, 
-						(byte) 199, (byte)  59, (byte) 155, (byte)  74, (byte) 237, (byte) 204, (byte) 146, (byte) 172, 
-						(byte) 227, (byte) 146, (byte) 126, (byte)  55, (byte) 245, (byte) 125, (byte)  12, (byte) 253, 
-						(byte)  94, (byte) 117, (byte) 129, (byte) 250, (byte)  81, (byte)  44, (byte) 143, (byte)  73, 
-						(byte)  97, (byte) 169, (byte) 235, (byte)  11, (byte) 128, (byte) 248, (byte) 168, (byte)   7,
-						(byte)  70, (byte) 114, (byte) 138, (byte)  85, (byte) 255, (byte)  70, (byte)  71, (byte)  31, 
-						(byte)  52, (byte)  37, (byte) 6,   (byte)  59, (byte) 157, (byte)  83, (byte) 100, (byte)  47, 
-						(byte)  94, (byte) 222, (byte)  30, (byte) 132, (byte) 214, (byte)  19, (byte)   8, (byte)  26, 
-						(byte) 250, (byte)  92, (byte)  34, (byte) 208, (byte)  81, (byte)  40, (byte)  91, (byte) 214, 
-						(byte)  59, (byte) 148, (byte)  59, (byte)  86, (byte)  93, (byte) 137, (byte) 138, (byte)   5, 
-						(byte) 104, (byte)  84, (byte)  19, (byte) 229, (byte)  60, (byte)  60, (byte) 108, (byte) 101, 
-						(byte)  37, (byte) 255, (byte)  31, (byte) 227, (byte)  78, (byte)  61, (byte) 220, (byte) 112, 
-						(byte) 240, (byte) 213, (byte) 100, (byte)  80, (byte) 253, (byte) 164, (byte) 139, (byte) 161, 
-						(byte)  46, (byte)  16, (byte)  78, (byte) 157, (byte) 235, (byte) 159, (byte) 184, (byte)  24,
-						(byte) 129, (byte) 225, (byte) 196, (byte) 189, (byte) 242, (byte)  93, (byte) 146, (byte)  71, 
-						(byte) 244, (byte)  80, (byte) 200, (byte) 101, (byte) 146, (byte) 121, (byte) 104, (byte) 231, 
-						(byte) 115, (byte)  52, (byte) 244, (byte)  65, (byte)  79, (byte) 117, (byte) 167, (byte)  80, 
-						(byte) 225, (byte)  57, (byte)  84, (byte) 110, (byte)  58, (byte) 138, (byte) 115, (byte) 157 };
+	private static final byte[] modPriv = { 
+		(byte) 84, (byte) 80, (byte)150, (byte) 58, (byte)165, (byte)235, (byte)242, (byte)123, 
+		(byte)217, (byte) 55, (byte) 38, (byte)154, (byte) 36, (byte)181, (byte)221, (byte)156, 
+		(byte)211, (byte)215, (byte)100, (byte)164, (byte) 90, (byte) 88, (byte) 40, (byte)228, 
+		(byte) 83, (byte)148, (byte) 54, (byte)122, (byte)  4, (byte) 16, (byte)165, (byte) 48, 
+		(byte) 76, (byte)194, (byte) 26, (byte)107, (byte) 51, (byte) 53, (byte)179, (byte)165, 
+		(byte) 31, (byte) 18, (byte)198, (byte)173, (byte) 78, (byte) 61, (byte) 56, (byte) 97, 
+		(byte)252, (byte)158, (byte)140, (byte) 80, (byte) 63, (byte) 25, (byte)223, (byte)156, 
+		(byte) 36, (byte)203, (byte)214, (byte)252, (byte)120, (byte) 67, (byte)180, (byte)167, 
+		(byte)  3, (byte) 82, (byte)243, (byte) 25, (byte) 97, (byte)214, (byte) 83, (byte)133, 
+		(byte) 69, (byte) 16, (byte)104, (byte) 54, (byte)160, (byte)200, (byte) 41, (byte) 83, 
+		(byte)164, (byte)187, (byte) 70, (byte)153, (byte)111, (byte)234, (byte)242, (byte)158, 
+		(byte)175, (byte) 28, (byte)198, (byte) 48, (byte)211, (byte) 45, (byte)148, (byte) 58, 
+		(byte) 23, (byte) 62, (byte)227, (byte) 74, (byte) 52, (byte)117, (byte) 42, (byte) 90, 
+		(byte) 41, (byte)249, (byte)130, (byte)154, (byte) 80, (byte)119, (byte) 61, (byte) 26, 
+		(byte)193, (byte) 40, (byte)125, (byte) 10, (byte)152, (byte)174, (byte)227, (byte)225, 
+		(byte)205, (byte) 32, (byte) 62, (byte) 66, (byte)  6, (byte)163, (byte)100, (byte) 99, 
+		(byte)219, (byte) 19, (byte)253, (byte) 25, (byte)105, (byte) 80, (byte)201, (byte) 29, 
+		(byte)252, (byte)157, (byte)237, (byte) 69, (byte)  1, (byte) 80, (byte)171, (byte)167, 
+		(byte) 20, (byte)196, (byte)156, (byte)109, (byte)249, (byte) 88, (byte)  0, (byte)  3, 
+		(byte)152, (byte) 38, (byte)165, (byte) 72, (byte) 87, (byte)  6, (byte)152, (byte) 71, 
+		(byte)156, (byte)214, (byte) 16, (byte) 71, (byte) 30, (byte) 82, (byte) 51, (byte)103, 
+		(byte) 76, (byte)218, (byte) 63, (byte)  9, (byte) 84, (byte)163, (byte)249, (byte) 91, 
+		(byte)215, (byte) 44, (byte)238, (byte) 85, (byte)101, (byte)240, (byte)148, (byte)  1, 
+		(byte) 82, (byte)224, (byte) 91, (byte)135, (byte)105, (byte)127, (byte) 84, (byte)171, 
+		(byte)181, (byte)152, (byte)210, (byte)183, (byte)126, (byte) 24, (byte) 46, (byte)196, 
+		(byte) 90, (byte)173, (byte) 38, (byte)245, (byte)219, (byte)186, (byte)222, (byte) 27, 
+		(byte)240, (byte)212, (byte)194, (byte) 15, (byte) 66, (byte)135, (byte)226, (byte)178, 
+		(byte)190, (byte) 52, (byte)245, (byte) 74, (byte) 65, (byte)224, (byte) 81, (byte)100, 
+		(byte) 85, (byte) 25, (byte)204, (byte)165, (byte)203, (byte)187, (byte)175, (byte) 84, 
+		(byte)100, (byte) 82, (byte) 15, (byte) 11, (byte) 23, (byte)202, (byte)151, (byte)107, 
+		(byte) 54, (byte) 41, (byte)207, (byte)  3, (byte)136, (byte)229, (byte)134, (byte)131, 
+		(byte) 93, (byte)139, (byte) 50, (byte)182, (byte)204, (byte) 93, (byte)130, (byte)89   };
 
 
 	private static RSAPublicKey publicKey;
@@ -109,7 +112,6 @@ public class RSA1_5Test extends TestCase {
 
 
 	static {
-	
 		try {
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 			
@@ -124,6 +126,7 @@ public class RSA1_5Test extends TestCase {
 			System.err.println(e);
 		}
 	}
+
 
 	public void testWithA128GCM()
 		throws Exception {
--- src/test/java/com/nimbusds/jose/crypto/RSASSATest.java
+++ src/test/java/com/nimbusds/jose/crypto/RSASSATest.java
@@ -25,7 +25,8 @@ import com.nimbusds.jose.util.Base64URL;
 
 
 /**
- * Tests RS256 JWS signing and verfication. Uses test vectors from JWS spec.
+ * Tests RS256 JWS signing and verfication. Uses test RSA keys and vectors 
+ * from the JWS spec.
  *
  * @author Vladimir Dzhuvinov
  * @version $version$ (2012-10-23)
@@ -33,75 +34,77 @@ import com.nimbusds.jose.util.Base64URL;
 public class RSASSATest extends TestCase {
 
 
-	private final static byte[] mod = { (byte)161, (byte)248, (byte) 22, (byte) 10, (byte)226, (byte)227, (byte)201, (byte)180,
-                                            (byte)101, (byte)206, (byte)141, (byte) 45, (byte)101, (byte) 98, (byte) 99, (byte) 54, 
-					    (byte) 43, (byte)146, (byte)125, (byte)190, (byte) 41, (byte)225, (byte)240, (byte) 36, 
-					    (byte)119, (byte)252, (byte) 22, (byte) 37, (byte)204, (byte)144, (byte)161, (byte) 54, 
-					    (byte)227, (byte)139, (byte)217, (byte) 52, (byte)151, (byte)197, (byte)182, (byte)234, 
-					    (byte) 99, (byte)221, (byte)119, (byte) 17, (byte)230, (byte)124, (byte)116, (byte) 41, 
-					    (byte)249, (byte) 86, (byte)176, (byte)251, (byte)138, (byte)143, (byte)  8, (byte)154, 
-					    (byte)220, (byte) 75, (byte)105, (byte)137, (byte) 60, (byte)193, (byte) 51, (byte) 63, 
-					    (byte) 83, (byte)237, (byte)208, (byte) 25, (byte)184, (byte)119, (byte)132, (byte) 37, 
-					    (byte) 47, (byte)236, (byte)145, (byte) 79, (byte)228, (byte)133, (byte)119, (byte)105,
-					    (byte) 89, (byte) 75, (byte)234, (byte) 66, (byte)128, (byte)211, (byte) 44, (byte) 15, 
-					    (byte) 85, (byte)191, (byte) 98, (byte)148, (byte) 79, (byte) 19, (byte)  3, (byte)150, 
-					    (byte)188, (byte)110, (byte)155, (byte)223, (byte)110, (byte)189, (byte)210, (byte)189, 
-					    (byte)163, (byte)103, (byte)142, (byte)236, (byte)160, (byte)198, (byte)104, (byte)247, 
-					    (byte)  1, (byte)179, (byte)141, (byte)191, (byte)251, (byte) 56, (byte)200, (byte) 52, 
-					    (byte) 44, (byte)226, (byte)254, (byte)109, (byte) 39, (byte)250, (byte)222, (byte) 74, 
-					    (byte) 90, (byte) 72, (byte)116, (byte)151, (byte)157, (byte)212, (byte)185, (byte)207, 
-					    (byte)154, (byte)222, (byte)196, (byte)199, (byte) 91, (byte)  5, (byte)133, (byte) 44, 
-					    (byte) 44, (byte) 15, (byte) 94, (byte)248, (byte)165, (byte)193, (byte)117, (byte)  3,
-					    (byte)146, (byte)249, (byte) 68, (byte)232, (byte)237, (byte)100, (byte)193, (byte) 16, 
-					    (byte)198, (byte)182, (byte) 71, (byte) 96, (byte)154, (byte)164, (byte)120, (byte) 58, 
-					    (byte)235, (byte)156, (byte)108, (byte)154, (byte)215, (byte) 85, (byte) 49, (byte) 48, 
-					    (byte) 80, (byte) 99, (byte)139, (byte)131, (byte)102, (byte) 92, (byte)111, (byte)111, 
-					    (byte)122, (byte)130, (byte)163, (byte)150, (byte)112, (byte) 42, (byte) 31, (byte)100, 
-					    (byte) 27, (byte)130, (byte)211, (byte)235, (byte)242, (byte) 57, (byte) 34, (byte) 25, 
-					    (byte) 73, (byte) 31, (byte)182, (byte)134, (byte)135, (byte) 44, (byte) 87, (byte) 22, 
-					    (byte)245, (byte) 10, (byte)248, (byte) 53, (byte)141, (byte)154, (byte)139, (byte)157, 
-					    (byte) 23, (byte)195, (byte) 64, (byte)114, (byte)143, (byte)127, (byte)135, (byte)216,
-					    (byte)154, (byte) 24, (byte)216, (byte)252, (byte)171, (byte)103, (byte)173, (byte)132, 
-					    (byte) 89, (byte) 12, (byte) 46, (byte)207, (byte)117, (byte)147, (byte) 57, (byte) 54, 
-					    (byte) 60, (byte)  7, (byte)  3, (byte) 77, (byte)111, (byte) 96, (byte)111, (byte)158, 
-					    (byte) 33, (byte)224, (byte) 84, (byte) 86, (byte)202, (byte)229, (byte)233, (byte)161  };
+	private final static byte[] mod = { 
+		(byte)161, (byte)248, (byte) 22, (byte) 10, (byte)226, (byte)227, (byte)201, (byte)180,
+                (byte)101, (byte)206, (byte)141, (byte) 45, (byte)101, (byte) 98, (byte) 99, (byte) 54, 
+		(byte) 43, (byte)146, (byte)125, (byte)190, (byte) 41, (byte)225, (byte)240, (byte) 36, 
+		(byte)119, (byte)252, (byte) 22, (byte) 37, (byte)204, (byte)144, (byte)161, (byte) 54, 
+		(byte)227, (byte)139, (byte)217, (byte) 52, (byte)151, (byte)197, (byte)182, (byte)234, 
+		(byte) 99, (byte)221, (byte)119, (byte) 17, (byte)230, (byte)124, (byte)116, (byte) 41, 
+		(byte)249, (byte) 86, (byte)176, (byte)251, (byte)138, (byte)143, (byte)  8, (byte)154, 
+		(byte)220, (byte) 75, (byte)105, (byte)137, (byte) 60, (byte)193, (byte) 51, (byte) 63, 
+		(byte) 83, (byte)237, (byte)208, (byte) 25, (byte)184, (byte)119, (byte)132, (byte) 37, 
+		(byte) 47, (byte)236, (byte)145, (byte) 79, (byte)228, (byte)133, (byte)119, (byte)105,
+		(byte) 89, (byte) 75, (byte)234, (byte) 66, (byte)128, (byte)211, (byte) 44, (byte) 15, 
+		(byte) 85, (byte)191, (byte) 98, (byte)148, (byte) 79, (byte) 19, (byte)  3, (byte)150, 
+		(byte)188, (byte)110, (byte)155, (byte)223, (byte)110, (byte)189, (byte)210, (byte)189, 
+		(byte)163, (byte)103, (byte)142, (byte)236, (byte)160, (byte)198, (byte)104, (byte)247, 
+		(byte)  1, (byte)179, (byte)141, (byte)191, (byte)251, (byte) 56, (byte)200, (byte) 52, 
+		(byte) 44, (byte)226, (byte)254, (byte)109, (byte) 39, (byte)250, (byte)222, (byte) 74, 
+		(byte) 90, (byte) 72, (byte)116, (byte)151, (byte)157, (byte)212, (byte)185, (byte)207, 
+		(byte)154, (byte)222, (byte)196, (byte)199, (byte) 91, (byte)  5, (byte)133, (byte) 44, 
+		(byte) 44, (byte) 15, (byte) 94, (byte)248, (byte)165, (byte)193, (byte)117, (byte)  3,
+		(byte)146, (byte)249, (byte) 68, (byte)232, (byte)237, (byte)100, (byte)193, (byte) 16, 
+		(byte)198, (byte)182, (byte) 71, (byte) 96, (byte)154, (byte)164, (byte)120, (byte) 58, 
+		(byte)235, (byte)156, (byte)108, (byte)154, (byte)215, (byte) 85, (byte) 49, (byte) 48, 
+		(byte) 80, (byte) 99, (byte)139, (byte)131, (byte)102, (byte) 92, (byte)111, (byte)111, 
+		(byte)122, (byte)130, (byte)163, (byte)150, (byte)112, (byte) 42, (byte) 31, (byte)100, 
+		(byte) 27, (byte)130, (byte)211, (byte)235, (byte)242, (byte) 57, (byte) 34, (byte) 25, 
+		(byte) 73, (byte) 31, (byte)182, (byte)134, (byte)135, (byte) 44, (byte) 87, (byte) 22, 
+		(byte)245, (byte) 10, (byte)248, (byte) 53, (byte)141, (byte)154, (byte)139, (byte)157, 
+		(byte) 23, (byte)195, (byte) 64, (byte)114, (byte)143, (byte)127, (byte)135, (byte)216,
+		(byte)154, (byte) 24, (byte)216, (byte)252, (byte)171, (byte)103, (byte)173, (byte)132, 
+		(byte) 89, (byte) 12, (byte) 46, (byte)207, (byte)117, (byte)147, (byte) 57, (byte) 54, 
+		(byte) 60, (byte)  7, (byte)  3, (byte) 77, (byte)111, (byte) 96, (byte)111, (byte)158, 
+		(byte) 33, (byte)224, (byte) 84, (byte) 86, (byte)202, (byte)229, (byte)233, (byte)161  };
 
 		  
 	private static final byte[] exp= { 1, 0, 1 };
 	
 	
-	private static final byte[] modPriv = { (byte)  18, (byte) 174, (byte) 113, (byte) 164, (byte) 105, (byte) 205, (byte)  10, (byte)  43,
-	                                        (byte) 195, (byte) 126, (byte)  82, (byte) 108, (byte)  69, (byte)   0, (byte)  87, (byte)  31, 
-						(byte)  29, (byte)  97, (byte) 117, (byte)  29, (byte) 100, (byte) 233, (byte)  73, (byte) 112, 
-						(byte) 123, (byte)  98, (byte)  89, (byte)  15, (byte) 157, (byte)  11, (byte) 165, (byte) 124, 
-						(byte) 150, (byte)  60, (byte)  64, (byte)  30, (byte)  63, (byte) 207, (byte)  47, (byte)  44, 
-						(byte) 211, (byte) 189, (byte) 236, (byte) 136, (byte) 229, (byte)   3, (byte) 191, (byte) 198, 
-						(byte)  67, (byte) 155, (byte)  11, (byte)  40, (byte) 200, (byte)  47, (byte) 125, (byte)  55, 
-						(byte) 151, (byte) 103, (byte)  31, (byte)  82, (byte)  19, (byte) 238, (byte) 216, (byte) 193, 
-						(byte)  90, (byte)  37, (byte) 216, (byte) 213, (byte) 206, (byte) 160, (byte)   2, (byte)  94, 
-						(byte) 227, (byte) 171, (byte)  46, (byte) 139, (byte) 127, (byte) 121, (byte)  33, (byte) 111,
-						(byte) 198, (byte)  59, (byte) 234, (byte)  86, (byte)  39, (byte)  83, (byte) 180, (byte)  6, 
-						(byte)  68, (byte) 198, (byte) 161, (byte)  81, (byte)  39, (byte) 217, (byte) 178, (byte) 149, 
-						(byte)  69, (byte)  64, (byte) 160, (byte) 187, (byte) 225, (byte) 163, (byte)   5, (byte)  86, 
-						(byte) 152, (byte)  45, (byte)  78, (byte) 159, (byte) 222, (byte)  95, (byte) 100, (byte)  37, 
-						(byte) 241, (byte)  77, (byte)  75, (byte) 113, (byte)  52, (byte)  65, (byte) 181, (byte)  93, 
-						(byte) 199, (byte)  59, (byte) 155, (byte)  74, (byte) 237, (byte) 204, (byte) 146, (byte) 172, 
-						(byte) 227, (byte) 146, (byte) 126, (byte)  55, (byte) 245, (byte) 125, (byte)  12, (byte) 253, 
-						(byte)  94, (byte) 117, (byte) 129, (byte) 250, (byte)  81, (byte)  44, (byte) 143, (byte)  73, 
-						(byte)  97, (byte) 169, (byte) 235, (byte)  11, (byte) 128, (byte) 248, (byte) 168, (byte)   7,
-						(byte)  70, (byte) 114, (byte) 138, (byte)  85, (byte) 255, (byte)  70, (byte)  71, (byte)  31, 
-						(byte)  52, (byte)  37, (byte) 6,   (byte)  59, (byte) 157, (byte)  83, (byte) 100, (byte)  47, 
-						(byte)  94, (byte) 222, (byte)  30, (byte) 132, (byte) 214, (byte)  19, (byte)   8, (byte)  26, 
-						(byte) 250, (byte)  92, (byte)  34, (byte) 208, (byte)  81, (byte)  40, (byte)  91, (byte) 214, 
-						(byte)  59, (byte) 148, (byte)  59, (byte)  86, (byte)  93, (byte) 137, (byte) 138, (byte)   5, 
-						(byte) 104, (byte)  84, (byte)  19, (byte) 229, (byte)  60, (byte)  60, (byte) 108, (byte) 101, 
-						(byte)  37, (byte) 255, (byte)  31, (byte) 227, (byte)  78, (byte)  61, (byte) 220, (byte) 112, 
-						(byte) 240, (byte) 213, (byte) 100, (byte)  80, (byte) 253, (byte) 164, (byte) 139, (byte) 161, 
-						(byte)  46, (byte)  16, (byte)  78, (byte) 157, (byte) 235, (byte) 159, (byte) 184, (byte)  24,
-						(byte) 129, (byte) 225, (byte) 196, (byte) 189, (byte) 242, (byte)  93, (byte) 146, (byte)  71, 
-						(byte) 244, (byte)  80, (byte) 200, (byte) 101, (byte) 146, (byte) 121, (byte) 104, (byte) 231, 
-						(byte) 115, (byte)  52, (byte) 244, (byte)  65, (byte)  79, (byte) 117, (byte) 167, (byte)  80, 
-						(byte) 225, (byte)  57, (byte)  84, (byte) 110, (byte)  58, (byte) 138, (byte) 115, (byte) 157 };
+	private static final byte[] modPriv = { 
+		(byte)  18, (byte) 174, (byte) 113, (byte) 164, (byte) 105, (byte) 205, (byte)  10, (byte)  43,
+	        (byte) 195, (byte) 126, (byte)  82, (byte) 108, (byte)  69, (byte)   0, (byte)  87, (byte)  31, 
+		(byte)  29, (byte)  97, (byte) 117, (byte)  29, (byte) 100, (byte) 233, (byte)  73, (byte) 112, 
+		(byte) 123, (byte)  98, (byte)  89, (byte)  15, (byte) 157, (byte)  11, (byte) 165, (byte) 124, 
+		(byte) 150, (byte)  60, (byte)  64, (byte)  30, (byte)  63, (byte) 207, (byte)  47, (byte)  44, 
+		(byte) 211, (byte) 189, (byte) 236, (byte) 136, (byte) 229, (byte)   3, (byte) 191, (byte) 198, 
+		(byte)  67, (byte) 155, (byte)  11, (byte)  40, (byte) 200, (byte)  47, (byte) 125, (byte)  55, 
+		(byte) 151, (byte) 103, (byte)  31, (byte)  82, (byte)  19, (byte) 238, (byte) 216, (byte) 193, 
+		(byte)  90, (byte)  37, (byte) 216, (byte) 213, (byte) 206, (byte) 160, (byte)   2, (byte)  94, 
+		(byte) 227, (byte) 171, (byte)  46, (byte) 139, (byte) 127, (byte) 121, (byte)  33, (byte) 111,
+		(byte) 198, (byte)  59, (byte) 234, (byte)  86, (byte)  39, (byte)  83, (byte) 180, (byte)  6, 
+		(byte)  68, (byte) 198, (byte) 161, (byte)  81, (byte)  39, (byte) 217, (byte) 178, (byte) 149, 
+		(byte)  69, (byte)  64, (byte) 160, (byte) 187, (byte) 225, (byte) 163, (byte)   5, (byte)  86, 
+		(byte) 152, (byte)  45, (byte)  78, (byte) 159, (byte) 222, (byte)  95, (byte) 100, (byte)  37, 
+		(byte) 241, (byte)  77, (byte)  75, (byte) 113, (byte)  52, (byte)  65, (byte) 181, (byte)  93, 
+		(byte) 199, (byte)  59, (byte) 155, (byte)  74, (byte) 237, (byte) 204, (byte) 146, (byte) 172, 
+		(byte) 227, (byte) 146, (byte) 126, (byte)  55, (byte) 245, (byte) 125, (byte)  12, (byte) 253, 
+		(byte)  94, (byte) 117, (byte) 129, (byte) 250, (byte)  81, (byte)  44, (byte) 143, (byte)  73, 
+		(byte)  97, (byte) 169, (byte) 235, (byte)  11, (byte) 128, (byte) 248, (byte) 168, (byte)   7,
+		(byte)  70, (byte) 114, (byte) 138, (byte)  85, (byte) 255, (byte)  70, (byte)  71, (byte)  31, 
+		(byte)  52, (byte)  37, (byte) 6,   (byte)  59, (byte) 157, (byte)  83, (byte) 100, (byte)  47, 
+		(byte)  94, (byte) 222, (byte)  30, (byte) 132, (byte) 214, (byte)  19, (byte)   8, (byte)  26, 
+		(byte) 250, (byte)  92, (byte)  34, (byte) 208, (byte)  81, (byte)  40, (byte)  91, (byte) 214, 
+		(byte)  59, (byte) 148, (byte)  59, (byte)  86, (byte)  93, (byte) 137, (byte) 138, (byte)   5, 
+		(byte) 104, (byte)  84, (byte)  19, (byte) 229, (byte)  60, (byte)  60, (byte) 108, (byte) 101, 
+		(byte)  37, (byte) 255, (byte)  31, (byte) 227, (byte)  78, (byte)  61, (byte) 220, (byte) 112, 
+		(byte) 240, (byte) 213, (byte) 100, (byte)  80, (byte) 253, (byte) 164, (byte) 139, (byte) 161, 
+		(byte)  46, (byte)  16, (byte)  78, (byte) 157, (byte) 235, (byte) 159, (byte) 184, (byte)  24,
+		(byte) 129, (byte) 225, (byte) 196, (byte) 189, (byte) 242, (byte)  93, (byte) 146, (byte)  71, 
+		(byte) 244, (byte)  80, (byte) 200, (byte) 101, (byte) 146, (byte) 121, (byte) 104, (byte) 231, 
+		(byte) 115, (byte)  52, (byte) 244, (byte)  65, (byte)  79, (byte) 117, (byte) 167, (byte)  80, 
+		(byte) 225, (byte)  57, (byte)  84, (byte) 110, (byte)  58, (byte) 138, (byte) 115, (byte) 157 };
 
 
 	private static RSAPublicKey publicKey;
@@ -111,7 +114,6 @@ public class RSASSATest extends TestCase {
 
 
 	static {
-	
 		try {
 			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 			
