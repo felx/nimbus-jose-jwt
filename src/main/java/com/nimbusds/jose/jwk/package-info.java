d6c14aa95548001229ec89e536395e684fcb78af
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-03-19 09:38:42 +0200

    Adds JWK package info

---
+++ src/main/java/com/nimbusds/jose/jwk/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * JSON Web Key (JWK) classes.
+ *
+ * <p>This package provides representation, serialisation and parsing of
+ * Elliptic Curve (EC), RSA and symmetric JWKs.
+ *
+ * <p>References:
+ *
+ * <ul>
+ *     <li>http://tools.ietf.org/wg/jose/
+ * </ul>
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ ($version-date$)
+ */
+package com.nimbusds.jose.jwk;
