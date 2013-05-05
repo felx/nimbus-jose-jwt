b4909217e2720951320f8a7713879371c80300f8
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-05-05 20:44:30 +0300

    Updates EncryptionMethod test

--- src/test/java/com/nimbusds/jose/EncryptionMethodTest.java
+++ src/test/java/com/nimbusds/jose/EncryptionMethodTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
  * Tests the EncryptionMethod class.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2013-04-15)
+ * @version $version$ (2013-05-05)
  */
 public class EncryptionMethodTest extends TestCase {
 
@@ -16,12 +16,12 @@ public class EncryptionMethodTest extends TestCase {
 	public void testCMKLengths() {
 
 
-		assertEquals(256, EncryptionMethod.A128CBC_HS256.cmkBitLength());
+		assertEquals(256, EncryptionMethod.A128CBC_HS256.cekBitLength());
 
-		assertEquals(512, EncryptionMethod.A256CBC_HS512.cmkBitLength());
+		assertEquals(512, EncryptionMethod.A256CBC_HS512.cekBitLength());
 
-		assertEquals(128, EncryptionMethod.A128GCM.cmkBitLength());
+		assertEquals(128, EncryptionMethod.A128GCM.cekBitLength());
 
-		assertEquals(256, EncryptionMethod.A256GCM.cmkBitLength());
+		assertEquals(256, EncryptionMethod.A256GCM.cekBitLength());
 	}
 }
