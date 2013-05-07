dec5c3521f37ea1a5373aaae744c87ea3d29b21a
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-05-07 10:06:58 +0300

    Adds AuthenticatedCipherText test

---
+++ src/test/java/com/nimbusds/jose/crypto/AuthenticatedCipherTextTest.java
@@ -0,0 +1,29 @@
+package com.nimbusds.jose.crypto;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+
+/**
+ * Tests the authenticated cipher text wrapper.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2013-05-07)
+ */
+public class AuthenticatedCipherTextTest extends TestCase {
+
+
+	public void testRun() {
+
+		byte[] cipherText = {1, 2, 3, 4, 5};
+		byte[] authTag = {6, 7, 8, 9, 10};
+
+		AuthenticatedCipherText act = new AuthenticatedCipherText(cipherText, authTag);
+
+		Assert.assertArrayEquals(cipherText, act.getCipherText());
+
+		Assert.assertArrayEquals(authTag, act.getAuthenticationTag());
+	}
+}
