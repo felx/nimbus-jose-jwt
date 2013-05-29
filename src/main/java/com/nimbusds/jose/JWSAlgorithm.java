53fb3f981eb5986398a85df48b734d3063284efe
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-05-29 12:08:08 +0300

    Adds PS256 and PS512 JWS algorithm constants

--- src/main/java/com/nimbusds/jose/JWSAlgorithm.java
+++ src/main/java/com/nimbusds/jose/JWSAlgorithm.java
@@ -21,12 +21,14 @@ import net.jcip.annotations.Immutable;
  *     <li>{@link #ES256}
  *     <li>{@link #ES384}
  *     <li>{@link #ES512}
+ *     <li>{@link #PS256}
+ *     <li>{@link #PS512}
  * </ul>
  *
  * <p>Additional JWS algorithm names can be defined using the constructors.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2013-01-08)
+ * @version $version$ (2013-05-29)
  */
 @Immutable
 public final class JWSAlgorithm extends Algorithm {
@@ -51,19 +53,19 @@ public final class JWSAlgorithm extends Algorithm {
 
 
 	/**
-	 * RSA using SHA-256 hash algorithm (recommended).
+	 * RSASSA-PKCS-v1_5 using SHA-256 hash algorithm (recommended).
 	 */
 	public static final JWSAlgorithm RS256 = new JWSAlgorithm("RS256", Requirement.RECOMMENDED);
 
 
 	/**
-	 * RSA using SHA-384 hash algorithm (optional).
+	 * RSASSA-PKCS-v1_5 using SHA-384 hash algorithm (optional).
 	 */
 	public static final JWSAlgorithm RS384 = new JWSAlgorithm("RS384", Requirement.OPTIONAL);
 
 
 	/**
-	 * RSA using SHA-512 hash algorithm (optional).
+	 * RSASSA-PKCS-v1_5 using SHA-512 hash algorithm (optional).
 	 */
 	public static final JWSAlgorithm RS512 = new JWSAlgorithm("RS512", Requirement.OPTIONAL);
 
@@ -84,6 +86,18 @@ public final class JWSAlgorithm extends Algorithm {
 	 * ECDSA using P-521 curve and SHA-512 hash algorithm (optional).
 	 */
 	public static final JWSAlgorithm ES512 = new JWSAlgorithm("ES512", Requirement.OPTIONAL);
+
+
+	/**
+	 * RSASSA-PSS using SHA-256 hash algorithm (optional).
+	 */
+	public static final JWSAlgorithm PS256 = new JWSAlgorithm("PS256", Requirement.OPTIONAL);
+
+
+	/**
+	 * RSASSA-PSS using SHA-512 hash algorithm (optional).
+	 */
+	public static final JWSAlgorithm PS512 = new JWSAlgorithm("PS512", Requirement.OPTIONAL);
 
 
 	/**
@@ -120,24 +134,28 @@ public final class JWSAlgorithm extends Algorithm {
 	 */
 	public static JWSAlgorithm parse(final String s) {
 
-		if (s == HS256.getName()) {
+		if (s.equals(HS256.getName())) {
 			return HS256;
-		} else if (s == HS384.getName()) {
+		} else if (s.equals(HS384.getName())) {
 			return HS384;
-		} else if (s == HS512.getName()) {
+		} else if (s.equals(HS512.getName())) {
 			return HS512;
-		} else if (s == RS256.getName()) {
+		} else if (s.equals(RS256.getName())) {
 			return RS256;
-		} else if (s == RS384.getName()) {
+		} else if (s.equals(RS384.getName())) {
 			return RS384;
-		} else if (s == RS512.getName()) {
+		} else if (s.equals(RS512.getName())) {
 			return RS512;
-		} else if (s == ES256.getName()) {
+		} else if (s.equals(ES256.getName())) {
 			return ES256;
-		} else if (s == ES384.getName()) {
+		} else if (s.equals(ES384.getName())) {
 			return ES384;
-		} else if (s == ES512.getName()) {
+		} else if (s.equals(ES512.getName())) {
 			return ES512;
+		} else if (s.equals(PS256.getName())) {
+			return PS256;
+		} else if (s.equals(PS512.getName())) {
+			return PS512;
 		} else {
 			return new JWSAlgorithm(s);
 		}
---
+++ src/test/java/com/nimbusds/jose/JWSAlgorithmTest.java
@@ -0,0 +1,33 @@
+package com.nimbusds.jose;
+
+
+import junit.framework.TestCase;
+
+
+/**
+ * Tests the JWT Algorithm class.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2013-05-29)
+ */
+public class JWSAlgorithmTest extends TestCase {
+
+
+	public void testParse() {
+
+		assertEquals(JWSAlgorithm.HS256, JWSAlgorithm.parse("HS256"));
+		assertEquals(JWSAlgorithm.HS384, JWSAlgorithm.parse("HS384"));
+		assertEquals(JWSAlgorithm.HS512, JWSAlgorithm.parse("HS512"));
+
+		assertEquals(JWSAlgorithm.RS256, JWSAlgorithm.parse("RS256"));
+		assertEquals(JWSAlgorithm.RS384, JWSAlgorithm.parse("RS384"));
+		assertEquals(JWSAlgorithm.RS512, JWSAlgorithm.parse("RS512"));
+
+		assertEquals(JWSAlgorithm.ES256, JWSAlgorithm.parse("ES256"));
+		assertEquals(JWSAlgorithm.ES384, JWSAlgorithm.parse("ES384"));
+		assertEquals(JWSAlgorithm.ES512, JWSAlgorithm.parse("ES512"));
+
+		assertEquals(JWSAlgorithm.PS256, JWSAlgorithm.parse("PS256"));
+		assertEquals(JWSAlgorithm.PS512, JWSAlgorithm.parse("PS512"));
+	}
+}
