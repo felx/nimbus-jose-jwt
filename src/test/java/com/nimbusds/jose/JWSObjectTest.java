3f3648b1bac9a05dd86c2c3c6b1e581d048262fa
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2014-01-17 12:37:56 +0200

    Extends JWSObject JUnit test

--- src/test/java/com/nimbusds/jose/JWSObjectTest.java
+++ src/test/java/com/nimbusds/jose/JWSObjectTest.java
@@ -1,6 +1,7 @@
 package com.nimbusds.jose;
 
 
+import com.nimbusds.jose.crypto.MACSigner;
 import junit.framework.TestCase;
 
 import com.nimbusds.jose.util.Base64URL;
@@ -10,7 +11,7 @@ import com.nimbusds.jose.util.Base64URL;
  * Tests JWS object methods.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2013-07-15)
+ * @version $version$ (2014-01-17)
  */
 public class JWSObjectTest extends TestCase {
 
@@ -34,5 +35,28 @@ public class JWSObjectTest extends TestCase {
 		assertEquals(firstPart.toString() + ".abc.def", jws.getParsedString());
 
 		assertEquals(JWSObject.State.SIGNED, jws.getState());
+	}
+
+
+	public void testForImmutableHeader()
+		throws Exception {
+
+		JWSHeader header = new JWSHeader(JWSAlgorithm.HS256);
+
+		JWSObject jwsObject = new JWSObject(header, new Payload("Hello world!"));
+
+		Base64URL signingInput = Base64URL.encode(jwsObject.getSigningInput());
+
+		header.setKeyID("1");
+
+		assertTrue(signingInput.equals(Base64URL.encode(jwsObject.getSigningInput())));
+
+		jwsObject.sign(new MACSigner("1234567890abc"));
+
+		String output = jwsObject.serialize();
+
+		header.setKeyID("2");
+
+		assertEquals(output, jwsObject.serialize());
 	}
 }
