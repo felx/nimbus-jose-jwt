859ff53ce948a633ebbd066284493a827c5fe9c2
Cedric Staub <cs@squareup.com>
2014-01-22 18:18:33 -0800

    Port RSA_OAEP to use JCA interfaces
    This change ports the RSA_OAEP encryptor to use the JCA interfaces
    instead of using BouncyCastle for doing encryption/decryption.

--- src/main/java/com/nimbusds/jose/crypto/RSA_OAEP.java
+++ src/main/java/com/nimbusds/jose/crypto/RSA_OAEP.java
@@ -1,20 +1,16 @@
 package com.nimbusds.jose.crypto;
 
 
-import java.math.BigInteger;
 import java.security.Provider;
+import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import net.jcip.annotations.ThreadSafe;
-
-import org.bouncycastle.crypto.AsymmetricBlockCipher;
-import org.bouncycastle.crypto.encodings.OAEPEncoding;
-import org.bouncycastle.crypto.engines.RSAEngine;
-import org.bouncycastle.crypto.params.RSAKeyParameters;
 
 import com.nimbusds.jose.JOSEException;
 
@@ -46,27 +42,17 @@ class RSA_OAEP {
 		throws JOSEException {
 
 		try {
-			AsymmetricBlockCipher engine = new RSAEngine();
-
-			// JCA identifier RSA/ECB/OAEPWithSHA-1AndMGF1Padding ?
-			OAEPEncoding cipher = new OAEPEncoding(engine);
-
-			BigInteger mod = pub.getModulus();
-			BigInteger exp = pub.getPublicExponent();
-			RSAKeyParameters keyParams = new RSAKeyParameters(false, mod, exp);
-			cipher.init(true, keyParams);
-
-			int inputBlockSize = cipher.getInputBlockSize();
-			int outputBlockSize = cipher.getOutputBlockSize();
-
-			byte[] keyBytes = cek.getEncoded();
-
-			return cipher.processBlock(keyBytes, 0, keyBytes.length);
+			Cipher cipher = CipherHelper.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding", provider);
+			cipher.init(Cipher.ENCRYPT_MODE, pub, new SecureRandom());
+			return cipher.doFinal(cek.getEncoded());
 
 		} catch (Exception e) {
-
-			// org.bouncycastle.crypto.InvalidCipherTextException
-			throw new JOSEException("Couldn't encrypt Content Encryption Key (CEK): " + e.getMessage(), e);
+			// java.security.NoSuchAlgorithmException
+			// java.security.NoSuchPaddingException
+			// java.security.InvalidKeyException
+			// javax.crypto.IllegalBlockSizeException
+			// javax.crypto.BadPaddingException
+			throw new JOSEException(e.getMessage(), e);
 		}
 	}
 
@@ -88,21 +74,17 @@ class RSA_OAEP {
 		throws JOSEException {
 
 		try {
-			RSAEngine engine = new RSAEngine();
-			OAEPEncoding cipher = new OAEPEncoding(engine);
-			
-			BigInteger mod = priv.getModulus();
-			BigInteger exp = priv.getPrivateExponent();
-
-			RSAKeyParameters keyParams = new RSAKeyParameters(true, mod, exp);
-			cipher.init(false, keyParams);
-			byte[] secretKeyBytes = cipher.processBlock(encryptedCEK, 0, encryptedCEK.length);
-			return new SecretKeySpec(secretKeyBytes, "AES");
+			Cipher cipher = CipherHelper.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding", provider);
+			cipher.init(Cipher.DECRYPT_MODE, priv);
+			return new SecretKeySpec(cipher.doFinal(encryptedCEK), "AES");
 
 		} catch (Exception e) {
-
-			// org.bouncycastle.crypto.InvalidCipherTextException
-			throw new JOSEException("Couldn't decrypt Content Encryption Key (CEK): " + e.getMessage(), e);
+			// java.security.NoSuchAlgorithmException
+			// java.security.NoSuchPaddingException
+			// java.security.InvalidKeyException
+			// javax.crypto.IllegalBlockSizeException
+			// javax.crypto.BadPaddingException
+			throw new JOSEException(e.getMessage(), e);
 		}
 	}
 
