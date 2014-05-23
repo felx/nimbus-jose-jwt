c9acd940cfac97a3ecde6bfa7b380ae1962f6f71
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2014-05-23 14:01:34 +0300

    Adds JavaDocs to AlgorithmParametersHelper

--- src/main/java/com/nimbusds/jose/crypto/AlgorithmParametersHelper.java
+++ src/main/java/com/nimbusds/jose/crypto/AlgorithmParametersHelper.java
@@ -1,23 +1,42 @@
 package com.nimbusds.jose.crypto;
 
+
 import java.security.AlgorithmParameters;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
 
+
 /**
- * Utility for creating AlgorithmParameters objects with an optional provider
- * @author Justin Richer
+ * Utility for creating {@link java.security.AlgorithmParameters} objects with
+ * an optional JCA provider.
  *
+ * @author Justin Richer
  */
 class AlgorithmParametersHelper {
 
-	
-	public static AlgorithmParameters getInstance(String name, Provider provider) throws NoSuchAlgorithmException {
+
+	/**
+	 * Creates a new {@link java.security.AlgorithmParameters} instance.
+	 *
+	 * @param name     The name of the requested algorithm. Must not be
+	 *                 {@code null}.
+	 * @param provider The JCA provider, or {@code null} to use the default
+	 *                 one.
+	 *
+	 * @return The AlgorithmParameters instance.
+	 *
+	 * @throws NoSuchAlgorithmException If an AlgorithmParameterGeneratorSpi
+	 *                                  implementation for the specified
+	 *                                  algorithm is not available from the
+	 *                                  specified Provider object.
+	 */
+	public static AlgorithmParameters getInstance(final String name, final Provider provider)
+		throws NoSuchAlgorithmException {
+
 		if (provider == null) {
 			return AlgorithmParameters.getInstance(name);
 		} else {
 			return AlgorithmParameters.getInstance(name, provider);
 		}
 	}
-	
 }
