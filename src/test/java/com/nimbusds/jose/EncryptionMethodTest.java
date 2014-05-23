f771db7fd4e26d0a8d62f64eb0b2ccec823cf423
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2014-05-23 11:27:37 +0300

    Extends EncryptionMethod JUnit test (issue #90)

--- src/test/java/com/nimbusds/jose/EncryptionMethodTest.java
+++ src/test/java/com/nimbusds/jose/EncryptionMethodTest.java
@@ -8,7 +8,7 @@ import junit.framework.TestCase;
  * Tests the EncryptionMethod class.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2013-08-20)
+ * @version $version$ (2014-05-23)
  */
 public class EncryptionMethodTest extends TestCase {
 
@@ -22,5 +22,8 @@ public class EncryptionMethodTest extends TestCase {
 		assertEquals(128, EncryptionMethod.A128GCM.cekBitLength());
 		assertEquals(192, EncryptionMethod.A192GCM.cekBitLength());
 		assertEquals(256, EncryptionMethod.A256GCM.cekBitLength());
+
+		assertEquals(256, EncryptionMethod.A128CBC_HS256_DEPRECATED.cekBitLength());
+		assertEquals(512, EncryptionMethod.A256CBC_HS512_DEPRECATED.cekBitLength());
 	}
 }
