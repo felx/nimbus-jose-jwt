745fd3e1699152ae93503f62ca82df5f95458bed
Connect2id Support <support@connect2id.com>
2015-06-29 19:21:22 +0300

    Extends tests (iss #140)

--- src/test/java/com/nimbusds/jwt/proc/DefaultJWTProcessorTest.java
+++ src/test/java/com/nimbusds/jwt/proc/DefaultJWTProcessorTest.java
@@ -5,20 +5,20 @@ import java.security.Key;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
-import com.nimbusds.jose.crypto.DirectEncrypter;
-import com.nimbusds.jose.crypto.MACSigner;
-import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
-import com.nimbusds.jwt.*;
 import junit.framework.TestCase;
 
 import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.bc.BouncyCastleProviderSingleton;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.proc.*;
+import com.nimbusds.jwt.*;
 
 
 /**
@@ -250,6 +250,75 @@ public class DefaultJWTProcessorTest extends TestCase {
 			new DefaultJOSEProcessor().process(jwt.serialize(), null);
 		} catch (BadJOSEException e) {
 			assertEquals("Unsecured (plain) JWTs are rejected, extend class to handle", e.getMessage());
+		}
+	}
+
+
+	public void testNoJWSKeyCandidates()
+		throws Exception {
+
+		// See http://tools.ietf.org/html/rfc7515#appendix-A.1
+		String jws = "eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9"+
+			"."+
+			"eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt"+
+			"cGxlLmNvbS9pc19yb290Ijp0cnVlfQ"+
+			"."+
+			"dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk";
+
+		DefaultJWTProcessor processor = new DefaultJWTProcessor();
+
+		processor.setJWSKeySelector(new JWSKeySelector() {
+			@Override
+			public List<? extends Key> selectJWSKeys(JWSHeader header, SecurityContext context) {
+				return new LinkedList<>(); // empty
+			}
+		});
+
+		try {
+			processor.process(jws, null);
+		} catch (BadJOSEException e) {
+			assertEquals("Signed JWT rejected: No matching key(s) found", e.getMessage());
+		}
+	}
+
+
+	public void testNoJWEKeyCandidates()
+		throws Exception {
+
+		String jwt = "eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiY3R5IjoiSldU"+
+			"In0."+
+			"g_hEwksO1Ax8Qn7HoN-BVeBoa8FXe0kpyk_XdcSmxvcM5_P296JXXtoHISr_DD_M"+
+			"qewaQSH4dZOQHoUgKLeFly-9RI11TG-_Ge1bZFazBPwKC5lJ6OLANLMd0QSL4fYE"+
+			"b9ERe-epKYE3xb2jfY1AltHqBO-PM6j23Guj2yDKnFv6WO72tteVzm_2n17SBFvh"+
+			"DuR9a2nHTE67pe0XGBUS_TK7ecA-iVq5COeVdJR4U4VZGGlxRGPLRHvolVLEHx6D"+
+			"YyLpw30Ay9R6d68YCLi9FYTq3hIXPK_-dmPlOUlKvPr1GgJzRoeC9G5qCvdcHWsq"+
+			"JGTO_z3Wfo5zsqwkxruxwA."+
+			"UmVkbW9uZCBXQSA5ODA1Mg."+
+			"VwHERHPvCNcHHpTjkoigx3_ExK0Qc71RMEParpatm0X_qpg-w8kozSjfNIPPXiTB"+
+			"BLXR65CIPkFqz4l1Ae9w_uowKiwyi9acgVztAi-pSL8GQSXnaamh9kX1mdh3M_TT"+
+			"-FZGQFQsFhu0Z72gJKGdfGE-OE7hS1zuBD5oEUfk0Dmb0VzWEzpxxiSSBbBAzP10"+
+			"l56pPfAtrjEYw-7ygeMkwBl6Z_mLS6w6xUgKlvW6ULmkV-uLC4FUiyKECK4e3WZY"+
+			"Kw1bpgIqGYsw2v_grHjszJZ-_I5uM-9RA8ycX9KqPRp9gc6pXmoU_-27ATs9XCvr"+
+			"ZXUtK2902AUzqpeEUJYjWWxSNsS-r1TJ1I-FMJ4XyAiGrfmo9hQPcNBYxPz3GQb2"+
+			"8Y5CLSQfNgKSGt0A4isp1hBUXBHAndgtcslt7ZoQJaKe_nNJgNliWtWpJ_ebuOpE"+
+			"l8jdhehdccnRMIwAmU1n7SPkmhIl1HlSOpvcvDfhUN5wuqU955vOBvfkBOh5A11U"+
+			"zBuo2WlgZ6hYi9-e3w29bR0C2-pp3jbqxEDw3iWaf2dc5b-LnR0FEYXvI_tYk5rd"+
+			"_J9N0mg0tQ6RbpxNEMNoA9QWk5lgdPvbh9BaO195abQ."+
+			"AVO9iT5AV4CzvDJCdhSFlQ";
+
+		DefaultJWTProcessor processor = new DefaultJWTProcessor();
+
+		processor.setJWEKeySelector(new JWEKeySelector() {
+			@Override
+			public List<? extends Key> selectJWEKeys(JWEHeader header, SecurityContext context) {
+				return new LinkedList<>(); // no candidates
+			}
+		});
+
+		try {
+			processor.process(jwt, null);
+		} catch (BadJOSEException e) {
+			assertEquals("JWE object rejected: No matching key(s) found", e.getMessage());
 		}
 	}
 }
