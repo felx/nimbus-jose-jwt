389f03d93d36c51a18a0df44cb04c7899747fd0c
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2016-08-17 13:57:28 +0300

    Extends ByteUtils tests

--- src/test/java/com/nimbusds/jose/util/ByteUtilsTest.java
+++ src/test/java/com/nimbusds/jose/util/ByteUtilsTest.java
@@ -1,9 +1,10 @@
 package com.nimbusds.jose.util;
 
 
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
 import java.util.Arrays;
 
-import com.nimbusds.jose.util.ByteUtils;
 import junit.framework.TestCase;
 
 
@@ -33,5 +34,19 @@ public class ByteUtilsTest extends TestCase {
 		byte[] out = ByteUtils.concat(a1, a2, a3);
 
 		assertTrue(Arrays.equals(new byte[]{(byte)1, (byte)2, (byte)3, (byte)4}, out));
+	}
+	
+	
+	public void testHashTruncation()
+		throws Exception {
+		
+		byte[] hash = MessageDigest.getInstance("SHA-256").digest("Hello, world!".getBytes(Charset.forName("UTF-8")));
+		
+		byte[] firstHalf  = ByteUtils.subArray(hash, ByteUtils.byteLength(0),   ByteUtils.byteLength(128));
+		byte[] secondHalf = ByteUtils.subArray(hash, ByteUtils.byteLength(128), ByteUtils.byteLength(128));
+		
+		byte[] concat = ByteUtils.concat(firstHalf, secondHalf);
+		
+		assertTrue(Base64URL.encode(hash).equals(Base64URL.encode(concat)));
 	}
 }
